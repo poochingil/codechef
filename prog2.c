0a76ce657aec0953dec3d4e7ae888e91a2ee038f
poochingil <siddhantgreat24@gmail.com>
2020-09-16 18:16:56 +0530

    done

---
+++ prog2.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main() {
+	int T,a,b,ans;
+	scanf("%d",&T);
+	while(T>0)
+	{
+	    
+	    scanf("%d%d",&a,&b);
+	    ans=a+b;
+	    printf("%d\n",ans);
+	    T--;
+	}
+	return 0;
+}
